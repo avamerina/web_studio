92a49e361a8da561b795b4016f998c396a517c2d
avamerina <avamerina@gmail.com>
2023-10-30 16:31:28 +0600

    order displayed info corrected

--- orders/serializers.py
+++ orders/serializers.py
@@ -6,7 +6,8 @@ from orders.models import Order, File
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        exclude = ['slug']
+        fields = '__all__'
+        read_only_fields = ['slug']
 
     def create(self, validated_data):
         return super().create(validated_data)
